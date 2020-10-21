26d1d6bc6ae1ae0c23a30c05605dec702f59e79d
unknown <sarah.barkley95@gmail.com>
2020-10-20 17:47:06 -0700

    challenge part 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,3 +2,7 @@ library(dplyr)
 df <- read.csv('MechaCar_mpg.csv')
 model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, df)
 summary(model)
+
+table <- read.csv('Suspension_Coil.csv')
+total_summary <- table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary <- table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
