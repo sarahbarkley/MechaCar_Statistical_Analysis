2046ff7554aa25ac83ca9294127fba599154d888
unknown <sarah.barkley95@gmail.com>
2020-10-22 19:19:32 -0700

    part 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,10 @@ summary(model)
 table <- read.csv('Suspension_Coil.csv')
 total_summary <- table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 lot_summary <- table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+t.test(table$PSI,mu=1500)
+t.test(subset(table$PSI,table$Manufacturing_Lot=="Lot1"),mu=1500)
+t.test(subset(table$PSI,table$Manufacturing_Lot=="Lot2"),mu=1500)
+t.test(subset(table$PSI,table$Manufacturing_Lot=="Lot3"),mu=1500)
+
+
